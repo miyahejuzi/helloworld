3bade91024fb0e3bc9bc500fb39466f6c9fd929c
miyahejuzi <2426105329@qq.com>
2019-08-24 11:24:33 +0800

    Initial commit

---
+++ src/main/java/cn/orange/helloworld/controller/HelloWorld.java
@@ -0,0 +1,17 @@
+package cn.orange.helloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author kz
+ * @date 2019/8/23
+ */
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+}
