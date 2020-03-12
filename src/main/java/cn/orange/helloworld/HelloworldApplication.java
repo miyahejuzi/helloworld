a518f4b4b617500fd907bce3873ebd6f42d4d790
miyahejuzi <2426105329@qq.com>
2020-03-12 15:46:20 +0800

    docker 阿里镜像自动拉取4

--- src/main/java/cn/orange/helloworld/HelloworldApplication.java
+++ src/main/java/cn/orange/helloworld/HelloworldApplication.java
@@ -3,6 +3,9 @@ package cn.orange.helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author orange
+ */
 @SpringBootApplication
 public class HelloworldApplication {
 
